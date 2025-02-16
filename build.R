ec419bb9e4e52ee47511c2b0b385907ba212c64b
pedro <pedrohenriquedrim@gmail.com>
2025-02-16 20:17:40 -0300

    correcao de pacote

--- build.R
+++ build.R
@@ -2,7 +2,7 @@
 load.packages = function() {
   require(compiler, quietly = TRUE)
   require(data.table, quietly = TRUE)
-  require(test_that, quietly = TRUE)
+  require(testthat, quietly = TRUE)
 }
 
 # Compilando funcoes
