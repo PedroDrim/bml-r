25659f205df96cb81101a2f6507742893c530c82
pedro <pedrohenriquedrim@gmail.com>
2025-02-16 20:53:58 -0300

    pequena correção

--- src/provider/MaxValueAnalysis.R
+++ src/provider/MaxValueAnalysis.R
@@ -1,7 +1,7 @@
 # Método responsável por obter o valor maximo de credit na lista de usuarios
 # @param userInfoList Lista de usuarios
 # @return Valor maximo de credit da lista
-maxValueAnalysis.analysis = function(userInfoList) {
+max.analysis = function(userInfoList) {
   creditVector = userInfoList$credit
   response = as.numeric(creditVector) |> max()
   return(response)
--- src/provider/MeanAnalysis.R
+++ src/provider/MeanAnalysis.R
@@ -1,7 +1,7 @@
 # Método responsável por obter a media dos valores de credit na lista de usuarios
 # @param userInfoList Lista de usuarios
 # @return Media de valores de credit da lista
-meanAnalysis.analysis = function(userInfoList) {
+mean.analysis = function(userInfoList) {
   creditVector = userInfoList$credit
   response = as.numeric(creditVector) |> mean()
   return(response)
