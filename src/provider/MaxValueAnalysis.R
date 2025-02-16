c613551845b10344480f80f308b6132640c31493
pedro <pedrohenriquedrim@gmail.com>
2025-02-16 20:58:55 -0300

    pequena correção

--- src/provider/MaxValueAnalysis.R
+++ src/provider/MaxValueAnalysis.R
@@ -1,7 +1,7 @@
 # Método responsável por obter o valor maximo de credit na lista de usuarios
 # @param userInfoList Lista de usuarios
 # @return Valor maximo de credit da lista
-max.analysis = function(userInfoList) {
+maxValueAnalysis.analysis = function(userInfoList) {
   creditVector = userInfoList$credit
   response = as.numeric(creditVector) |> max()
   return(response)
