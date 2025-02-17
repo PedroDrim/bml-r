9e034d9aa4d026705de77bbf69325cdb6c198278
pedro <pedrohenriquedrim@gmail.com>
2025-02-16 21:03:18 -0300

    pequena correção

--- src/provider/MeanAnalysis.R
+++ src/provider/MeanAnalysis.R
@@ -1,7 +1,7 @@
 # Método responsável por obter a media dos valores de credit na lista de usuarios
 # @param userInfoList Lista de usuarios
 # @return Media de valores de credit da lista
-mean.analysis = function(userInfoList) {
+meanAnalysis.analysis = function(userInfoList) {
   creditVector = userInfoList$credit
   response = as.numeric(creditVector) |> mean()
   return(response)
