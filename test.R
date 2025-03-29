e179e47479fd06a1479148acc127444fed2e1aaa
pedro <pedrohenriquedrim@gmail.com>
2025-03-29 18:44:00 -0300

    Organizando workflow

--- test.R
+++ test.R
@@ -3,5 +3,6 @@ source("./build.R")
 srcFiles = list.files(".//src", recursive = TRUE, full.names = TRUE, pattern = ".R$")
 testFiles = list.files(".//test", recursive = TRUE, full.names = TRUE, pattern = ".test.R$")
 
+dir.create(".//coverage")
 coverage_data = file_coverage(srcFiles, testFiles)
-report(coverage_data, file = "coverage_report.xml", browse = FALSE)
+report(coverage_data, file = ".//coverage//coverage_report.xml", browse = FALSE)
