adf52ad0e92e69811c267636ac296b4d48e863be
pedro <pedrohenriquedrim@gmail.com>
2025-03-29 14:28:33 -0300

    atualizando testes

--- test.R
+++ test.R
@@ -2,7 +2,7 @@ source("./build.R")
 
 srcFiles = list.files(".//src", recursive = TRUE, full.names = TRUE, pattern = ".R$")
 testFiles = list.files(".//test", recursive = TRUE, full.names = TRUE, pattern = ".test.R$")
-status = sapply(testFiles, test_file)
+status = sapply(testFiles, testFile)
 
 coverage_data = file_coverage(srcFiles, testFiles)
 report(coverage_data, file = "coverage_report.xml", browse = FALSE)
