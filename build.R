57dea3c0b55dce07ae193f804aad508bc7bb7ae1
pedro <pedrohenriquedrim@gmail.com>
2025-03-29 14:23:11 -0300

    atualizando testes

--- build.R
+++ build.R
@@ -3,6 +3,7 @@ load.packages = function() {
   require(compiler, quietly = TRUE)
   require(data.table, quietly = TRUE)
   require(testthat, quietly = TRUE)
+  require(covr, quietly = TRUE)
 }
 
 # Compilando funcoes
--- test.R
+++ test.R
@@ -1,4 +1,8 @@
 source("./build.R")
 
+srcFiles = list.files(".//src", recursive = TRUE, full.names = TRUE, pattern = ".R$")
 testFiles = list.files(".//test", recursive = TRUE, full.names = TRUE, pattern = ".test.R$")
-status = sapply(testFiles, source)
+status = sapply(testFiles, test_file)
+
+coverage_data = file_coverage(srcFiles, testFiles)
+report(coverage_data, file = "coverage_report.xml", browse = FALSE)
