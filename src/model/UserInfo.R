c9642e33153b7930a87b9b3834aea444d62e0d1e
pedro <pedrohenriquedrim@gmail.com>
2025-08-17 18:41:29 -0300

    gerando relatorio de resposta

--- src/model/UserInfo.R
+++ src/model/UserInfo.R
@@ -1,12 +1,15 @@
-# Informacoes do usuario
+# Metodo para criar Informacoes do usuario
+# @param (character[]) userArray
+# @param (character[]) passwordArray
+# @returns (data.table) Tabela contendo usuarios e senhas
 userInfo.create = function(userArray, passwordArray) {
   response = data.table(user = userArray, password = private.cryptPassword(passwordArray))
   return(response)
 }
 
 # Metodo privado para encriptar a senha do usuario
-# @param password Senha a ser encriptada
-# @return Nova senha encriptada
+# @param (character[]) passwordArray Senha a ser encriptada
+# @return (character) Nova senha encriptada
 private.cryptPassword = function(passwordArray) {
   cryptList = strsplit(passwordArray, NULL) |> sapply(function(crypt) {
     size = length(crypt)
