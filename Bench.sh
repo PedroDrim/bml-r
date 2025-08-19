601344fd73cb915322f8afc3e58df7abb9eea63e
pedro <pedrohenriquedrim@gmail.com>
2025-08-19 13:30:35 -0300

    teste de timeout

--- Bench.sh
+++ Bench.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "[INFO] Rodando start.R..."
 Rscript start.R config.json
-
-echo "[INFO] Conteúdo do diretório:"
-ls -lh
-
-if [ -f benchmark.json ]; then
-  echo "[INFO] benchmark.json encontrado, mostrando conteúdo:"
-  cat benchmark.json
-else
-  echo "[ERRO] benchmark.json não foi gerado!"
-  exit 1
-fi
-
+cat benchmark.json
